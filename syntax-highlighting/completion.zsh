edaf37caf1fe14af33e4274395366b46b100a95b
Sander Hurlen <sanderhurlen@Sanders-MacBook-Pro.local>
2020-02-24 19:21:58 +0100

    RENAME autocompletion and synta-hyghlighting files

---
+++ syntax-highlighting/completion.zsh
@@ -0,0 +1,5 @@
+# install into prompt
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# If it does not work, you may have to expose the directory path
+# export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
--- syntax-highlighting/install.zsh
+++
@@ -1,2 +0,0 @@
-# install into prompt
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
