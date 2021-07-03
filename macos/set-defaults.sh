8e453471e06d5cffe0c10cbdd2a272dfc02111b0
Sander Hurlen <sander.hurlen@icloud.com>
2021-07-03 12:28:59 +0200

    add key repeat default for vim

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -50,6 +50,13 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
+# To enable key-repeating execute the following in your Terminal and restart VS Code:
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+
+
 
 ########################
 #    DOCK
