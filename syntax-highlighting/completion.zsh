b9c57c797802dae2d9a0b6571d6c4942498745d9
Sander Hurlen <sander.hurlen@icloud.com>
2024-05-02 13:39:31 +0200

    fix path for syntax hightlighting

--- syntax-highlighting/completion.zsh
+++ syntax-highlighting/completion.zsh
@@ -1,5 +1,5 @@
 # install into prompt
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # If it does not work, you may have to expose the directory path
 # export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
