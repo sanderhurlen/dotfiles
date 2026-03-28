213b0bac3055a144e746405fe26d1eee034ee3e2
Sander Hurlen <sander.hurlen@icloud.com>
2026-03-28 09:44:13 +0100

    add nvm install and pnpm setup

--- nvm/install.sh
+++ nvm/install.sh
@@ -5,3 +5,22 @@ then
   echo "Creating ~/.nvm"
   mkdir ~/.nvm
 fi
+
+# Install nvm
+if ! test -f ~/.nvm/nvm.sh
+then
+  echo "Installing nvm..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+fi
+
+# Install latest LTS node
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+if ! nvm ls --no-colors | grep -q "lts/"
+then
+  echo "Installing Node LTS..."
+  nvm install --lts
+  nvm use --lts
+  nvm alias default node
+fi
